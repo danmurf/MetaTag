aea5ddcadf938f1d06ce2a34a9468507e855d5d7
Dan Murfitt <dan@murfitt.net>
2016-11-15 21:04:36 +0000

    Added initial test placeholders and started including docblocks for code coverage.

--- tests/MetaTagTest.php
+++ tests/MetaTagTest.php
@@ -1,22 +1,72 @@
 <?php
 
-use danmurf\MetaTag as MetaTag;
+use danmurf\MetaTag;
 
-class MetaTagTest extends TestCase {
-
+class MetaTagTest extends TestCase
+{
     /**
      * @test
-     */
-    public function testing_setup_works_ok() {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * @test
+     * @covers \danmurf\MetaTag::keywords
      */
      public function can_set_keywords()
      {
          $metatag = new MetaTag();
          $this->assertNull($metatag->keywords(['first', 'second', 'third']));
+     }
+
+     /**
+      * @test
+      */
+     public function can_render_keywords()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_set_no_index()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_render_no_index()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_set_description()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_render_description()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_set_author()
+     {
+         //@ToDo
+     }
+
+     /**
+      * @test
+      */
+     public function can_render_author()
+     {
+         //@ToDo
      }
 }
