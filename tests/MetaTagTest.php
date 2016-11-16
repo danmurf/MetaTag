a0c64c58ddef2f2344ae5c4b38fb190374df20d4
Dan Murfitt <dan@murfitt.net>
2016-11-16 20:44:35 +0000

    Added final tests.

--- tests/MetaTagTest.php
+++ tests/MetaTagTest.php
@@ -16,6 +16,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::render_keywords
       */
      public function can_render_keywords()
      {
@@ -30,6 +31,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::noindex
       */
      public function can_set_no_index()
      {
@@ -40,6 +42,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::render_noindex
       */
      public function can_render_no_index()
      {
@@ -54,6 +57,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::description
       */
      public function can_set_description()
      {
@@ -64,6 +68,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::render_description
       */
      public function can_render_description()
      {
@@ -78,6 +83,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::author
       */
      public function can_set_author()
      {
@@ -88,6 +94,7 @@ class MetaTagTest extends TestCase
 
      /**
       * @test
+      * @covers \danmurf\MetaTag::render_author
       */
      public function can_render_author()
      {
@@ -98,5 +105,44 @@ class MetaTagTest extends TestCase
          //Check it renders correctly
          $expected_result = '<meta name="author" content="Jessie Wongus">'."\n";
          $this->assertEquals($expected_result, $metatag->render_author());
+     }
+
+     /**
+      * @test
+      * @covers \danmurf\MetaTag::sanitise_content
+      */
+     public function content_is_properly_sanitised()
+     {
+         //Set the author tag
+         $metatag = new MetaTag();
+         $metatag->description('Here is a string with "double quotes" <tags> and a new
+         line');
+
+         //Check it renders correctly
+         $expected_result = '<meta name="description" content="Here is a string with double quotes and a new line">'."\n";
+         $this->assertEquals($expected_result, $metatag->render_description());
+     }
+
+     /**
+      * @test
+      * @covers \danmurf\MetaTag::render
+      * @covers \danmurf\MetaTag::render_meta_tag
+      */
+     public function all_tags_render_together()
+     {
+         //Set the tags
+         $metatag = new MetaTag();
+         $metatag->noindex(true);
+         $metatag->keywords(['first', 'second', 'third']);
+         $metatag->description("This is a description of the webpage.");
+         $metatag->author("Jessie Wongus");
+
+         //Build the expected results with 4 spaces indented
+         $expected_result =  '    <meta name="robots" content="noindex, noarchive, nofollow">'."\n";
+         $expected_result .= '    <meta name="keywords" content="first, second, third">'."\n";
+         $expected_result .= '    <meta name="description" content="This is a description of the webpage.">'."\n";
+         $expected_result .= '    <meta name="author" content="Jessie Wongus">'."\n";
+
+         $this->assertEquals($expected_result, $metatag->render(4)); //Render the results with 4 spaces indented
      }
 }
