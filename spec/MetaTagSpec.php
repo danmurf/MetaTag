21543659064107e531fb66bb8db87c153e1bf064
Dan Murfitt <dan@murfitt.net>
2018-03-12 20:48:43 +0000

    Add spec tests and fix implementation.

--- spec/MetaTagSpec.php
+++ spec/MetaTagSpec.php
@@ -4,17 +4,128 @@ namespace spec\danmurf;
 
 use danmurf\MetaTag;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class MetaTagSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(MetaTag::class);
     }
 
-    function it_can_set_no_index()
+    public function it_can_set_no_index()
     {
         $this->setNoIndex(true)->shouldReturn($this);
+    }
+
+    public function it_can_set_keywords()
+    {
+        $this->setKeywords([])->shouldReturn($this);
+    }
+
+    public function it_can_set_description()
+    {
+        $this->setDescription('Some description')->shouldReturn($this);
+    }
+
+    public function it_can_set_author()
+    {
+        $this->setAuthor('Jessie Wongus')->shouldReturn($this);
+    }
+
+    public function it_can_set_canonical_url()
+    {
+        $this->setCanonicalUrl('http://example.com')->shouldReturn($this);
+    }
+
+    public function it_can_render_no_index()
+    {
+        $this->renderNoIndex()->shouldReturn(null);
+
+        $this->setNoindex(true);
+
+        $expectedResult = '<meta name="robots" content="noindex, noarchive, nofollow">'."\n";
+        $this->renderNoIndex()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_render_keywords()
+    {
+        $this->renderKeywords()->shouldReturn(null);
+
+        $this->setKeywords(['first', 'second', 'third']);
+
+        $expectedResult = '<meta name="keywords" content="first, second, third">'."\n";
+        $this->renderKeywords()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_render_description()
+    {
+        $this->renderDescription()->shouldReturn(null);
+
+        $this->setDescription('This is a description of the webpage.');
+        $expectedResult =
+        '<meta name="description" content="This is a description of the webpage.">'."\n";
+        $this->renderDescription()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_render_author()
+    {
+        $this->renderAuthor()->shouldReturn(null);
+
+        $this->setAuthor('Jessie Wongus');
+        $expectedResult = '<meta name="author" content="Jessie Wongus">'."\n";
+        $this->renderAuthor()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_render_canonical_url()
+    {
+        $this->renderCanonicalUrl()->shouldReturn(null);
+
+        $this->setCanonicalUrl('https://www.example.com/testing?q=hello-world');
+        $expectedResult = '<link rel="canonical" href="https://www.example.com/testing?q=hello-world">'."\n";
+        $this->renderCanonicalUrl()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_properly_sanitise_content()
+    {
+        $dirtyContent = 'Here is a string with "double quotes" <tags> and a new
+        line';
+
+        $expectedResult = '<meta name="description" content="Here is a string with double quotes and a new line">'."\n";
+        $this->setDescription($dirtyContent);
+        $this->renderDescription()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_render_all_tags_together()
+    {
+        $this->setNoIndex(true)
+            ->setKeywords(['first', 'second', 'third'])
+            ->setDescription('This is a description of the webpage.')
+            ->setAuthor('Jessie Wongus')
+            ->setCanonicalUrl('https://www.example.com/testing?q=hello-world');
+
+        $expectedResult = '<meta name="robots" content="noindex, noarchive, nofollow">'."\n";
+        $expectedResult .= '<meta name="keywords" content="first, second, third">'."\n";
+        $expectedResult .= '<meta name="description" content="This is a description of the webpage.">'."\n";
+        $expectedResult .= '<meta name="author" content="Jessie Wongus">'."\n";
+        $expectedResult .= '<link rel="canonical" href="https://www.example.com/testing?q=hello-world">'."\n";
+
+        $this->render()->shouldReturn($expectedResult);
+    }
+
+    public function it_can_indent_rendered_tags()
+    {
+        $this->setNoIndex(true)
+        ->setKeywords(['first', 'second', 'third'])
+        ->setDescription('This is a description of the webpage.')
+        ->setAuthor('Jessie Wongus')
+        ->setCanonicalUrl('https://www.example.com/testing?q=hello-world');
+
+        $expectedResult = '    <meta name="robots" content="noindex, noarchive, nofollow">'."\n";
+        $expectedResult .= '    <meta name="keywords" content="first, second, third">'."\n";
+        $expectedResult .= '    <meta name="description" content="This is a description of the webpage.">'."\n";
+        $expectedResult .= '    <meta name="author" content="Jessie Wongus">'."\n";
+        $expectedResult .= '    <link rel="canonical" href="https://www.example.com/testing?q=hello-world">'."\n";
+
+        $this->render(4)->shouldReturn($expectedResult);
     }
 }
--- src/MetaTag.php
+++ src/MetaTag.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 namespace danmurf;
 
 /**
  * Meta tag library for accumulating and rendering HTML head meta tags.
- * 
+ *
  * @author Dan Murfitt <dan@murfitt.net>
  */
 class MetaTag
@@ -15,7 +15,7 @@ class MetaTag
     protected $prefix;
 
     /**
-     * @var boolean
+     * @var bool
      */
     protected $noIndex;
 
@@ -41,96 +41,54 @@ class MetaTag
 
     /**
      * Output the meta tags.
-     * 
-     * @param int $indentSpaces The number of spaces to indent the tags, for tidyness.
-     * 
-     * @return string All the completed meta tags for the head section.
+     *
+     * @param int $indentSpaces the number of spaces to indent the tags, for tidyness
+     *
+     * @return string all the completed meta tags for the head section
      */
-    public function render($indentSpaces = 0): string
+    public function render(int $indentSpaces = 0): string
     {
-        $this->prefix = str_repeat(" ", $indentSpaces);
+        $this->prefix = str_repeat(' ', $indentSpaces);
 
-        $output = "";
-        $output .=  $this->renderNoindex();
-        $output .=  $this->renderKeywords();
-        $output .=  $this->renderDescription();
-        $output .=  $this->renderAuthor();
-        $output .=  $this->renderCanonical();
+        $output = '';
+        $output .= $this->renderNoindex();
+        $output .= $this->renderKeywords();
+        $output .= $this->renderDescription();
+        $output .= $this->renderAuthor();
+        $output .= $this->renderCanonicalUrl();
 
         return $output;
     }
 
     /**
-     * Render a head tag.
-     * 
-     * @param string $type The type of tag, e.g. meta, or link.
-     * @param array $attributes A key value array of attributes
-     * 
-     * @return string The head tag string.
-     */
-    private function renderTag(string $type, array $attributes = []): string
-    {
-        $tag = $this->prefix . '<' . $type;
-
-        foreach ($attributes as $attribute => $value) {
-            $tag .= ' ' . $attribute . '="' . $value . '"';
-        }
-
-        $tag .= ">\n";
-
-        return $tag;
-    }
-
-    /**
-     * Clean the content so it is suitable for a meta tag content field.
-     * 
-     * @param string $content Dirty string, with tags, quotes and what not.
-     * 
-     * @return string Clean string, which won't break tags and will look nice.
-     */
-    private function sanitiseContent($content): string
-    {
-        //Remove any tags
-        $content = strip_tags($content);
-
-        //Remove newlines from content
-        $content = trim(preg_replace('/\s+/', ' ', $content));
-
-        //Remove any double quotes
-        $content = str_replace('"', '', $content);
-
-        return $content;
-    }
-
-    /**
      * Whether to add a noindex tag to the page.
-     * 
-     * @param boolean $value True if you would like the page not to be indexed.
-     * 
+     *
+     * @param bool $value true if you would like the page not to be indexed
+     *
      * @return MetaTag
      */
     public function setNoIndex(bool $value): MetaTag
     {
-        $this->noindex = $value;
+        $this->noIndex = $value;
 
         return $this;
     }
 
     /**
      * Render the noindex tag.
-     * 
-     * @return string The final noindex tag string.
+     *
+     * @return string the final noindex tag string
      */
-    public function renderNoindex(): string
+    public function renderNoindex(): ?string
     {
-        return $this->noIndex == true ? $this->renderTag('meta', ['name' => 'robots', 'content' => 'noindex, noarchive, nofollow']) : null;
+        return true == $this->noIndex ? $this->renderTag('meta', ['name' => 'robots', 'content' => 'noindex, noarchive, nofollow']) : null;
     }
 
     /**
      * Set the page meta keywords.
-     * 
-     * @param array $keywords An array of keywords to include.
-     * 
+     *
+     * @param array $keywords an array of keywords to include
+     *
      * @return MetaTag
      */
     public function setKeywords(array $keywords): MetaTag
@@ -142,19 +100,19 @@ class MetaTag
 
     /**
      * Render the keywords tag.
-     * 
-     * @return string The final keywords meta tag.
+     *
+     * @return string the final keywords meta tag
      */
-    public function renderKeywords(): string
+    public function renderKeywords(): ?string
     {
-        return sizeof($this->keywords) > 0 ? $this->renderTag('meta', ['name' => 'keywords', 'content' => $this->sanitiseContent(implode(", ", $this->keywords))]) : null;
+        return null !== $this->keywords && sizeof($this->keywords) > 0 ? $this->renderTag('meta', ['name' => 'keywords', 'content' => $this->sanitiseContent(implode(', ', $this->keywords))]) : null;
     }
 
     /**
      * Set the page meta description.
-     * 
-     * @param string $description A description of the page.
-     * 
+     *
+     * @param string $description a description of the page
+     *
      * @return MetaTag
      */
     public function setDescription($description): MetaTag
@@ -166,19 +124,19 @@ class MetaTag
 
     /**
      * Render the description meta tag.
-     * 
-     * @return string The final description meta tag string.
+     *
+     * @return string the final description meta tag string
      */
-    public function renderDescription(): string
+    public function renderDescription(): ?string
     {
         return strlen($this->description) > 0 ? $this->renderTag('meta', ['name' => 'description', 'content' => $this->sanitiseContent($this->description)]) : null;
     }
 
     /**
      * Set the page's author meta tag.
-     * 
-     * @param string $author The name of the author.
-     * 
+     *
+     * @param string $author the name of the author
+     *
      * @return MetaTag
      */
     public function setAuthor($author): MetaTag
@@ -190,19 +148,19 @@ class MetaTag
 
     /**
      * Render the author meta tag.
-     * 
-     * @return string The final author meta tag.
+     *
+     * @return string the final author meta tag
      */
-    public function renderAuthor(): string
+    public function renderAuthor(): ?string
     {
         return strlen($this->author) > 0 ? $this->renderTag('meta', ['name' => 'author', 'content' => $this->sanitiseContent($this->author)]) : null;
     }
 
     /**
      * Set the page's canonical URL.
-     * 
-     * @param string $url The page's canonical URL.
-     * 
+     *
+     * @param string $url the page's canonical URL
+     *
      * @return MetaTag
      */
     public function setCanonicalUrl(string $url): MetaTag
@@ -214,11 +172,53 @@ class MetaTag
 
     /**
      * Render the canonical URL link tag.
-     * 
-     * @return string The page's canonical URL tag.
+     *
+     * @return string the page's canonical URL tag
      */
-    public function renderCanonical(): string
+    public function renderCanonicalUrl(): ?string
     {
         return strlen($this->canonicalUrl) > 0 ? $this->renderTag('link', ['rel' => 'canonical', 'href' => $this->canonicalUrl]) : null;
+    }
+
+    /**
+     * Render a head tag.
+     *
+     * @param string $type       The type of tag, e.g. meta, or link.
+     * @param array  $attributes A key value array of attributes
+     *
+     * @return string the head tag string
+     */
+    private function renderTag(string $type, array $attributes = []): string
+    {
+        $tag = $this->prefix.'<'.$type;
+
+        foreach ($attributes as $attribute => $value) {
+            $tag .= ' '.$attribute.'="'.$value.'"';
+        }
+
+        $tag .= ">\n";
+
+        return $tag;
+    }
+
+    /**
+     * Clean the content so it is suitable for a meta tag content field.
+     *
+     * @param string $content dirty string, with tags, quotes and what not
+     *
+     * @return string clean string, which won't break tags and will look nice
+     */
+    private function sanitiseContent(string $content): string
+    {
+        //Remove any tags
+        $content = strip_tags($content);
+
+        //Remove newlines from content
+        $content = trim(preg_replace('/\s+/', ' ', $content));
+
+        //Remove any double quotes
+        $content = str_replace('"', '', $content);
+
+        return $content;
     }
 }
